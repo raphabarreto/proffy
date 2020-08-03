e06bcaddd7e8790f1320e12d7f564ab72478bde8
Raphael Barreto <tnt.raphael@gmail.com>
2020-08-03 15:44:25 -0300

    Finalizado layout e componentes - Dia 1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import GlobalStyles from './styles/global';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  );
 };
 
 export default App;
---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars3.githubusercontent.com/u/6462941?s=460&v=4"
+          alt="Raphael Barreto"
+        />
+        <div>
+          <strong>Raphael Barreto</strong>
+          <span>Matemática</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
